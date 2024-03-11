42cabacf044e954fad61a4c8d05278cd01c2369b
kashyap vishnudas <kashyapvishnudas@gmail.com>
2024-03-12 02:36:54 +0530

    32:00min

---
+++ 1-into.js
@@ -0,0 +1,3 @@
+const amount = 12;
+amount < 10 ? console.log('small no') : console.log('large no');
+console.log('here is my first node app');
---
+++ 2-intro.js
@@ -0,0 +1,9 @@
+// GLOBALS
+console.log(__dirname);
+console.log(__filename);
+// require
+// module
+// progress
+setInterval(() => {
+  console.log('hello world');
+}, 1000);
